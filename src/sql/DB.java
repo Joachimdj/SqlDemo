4406160405af67570ee075f0fb8ae72b91ffdb32
Mathias <Mathias@10.50.131.125>
2014-09-19 15:14:19 +0200

    insertCol added

---
+++ src/sql/DB.java
@@ -0,0 +1,8 @@
+package sql;
+
+public class DB {
+	public static String driver = "oracle.jdbc.driver.OracleDriver";
+	public static String URL    = "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat";
+	public static String ID     = "cphmp259";
+	public static String PW     = "cphmp259";
+}
